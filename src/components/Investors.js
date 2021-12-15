ce0caaab000f88711246bc901c34287516333999
jamelhfarhan <jamelh.farhan.rae.js@tuwaiq.edu.sa>
2021-12-16 00:36:45 +0300

    seacendd

--- src/components/Investors.js
+++ src/components/Investors.js
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function Investors() {
+export default function Investors({token}) {
+
+const [Investors, setInvestors] = useState([])
+const [name, setname1] = useState("");
+const [email, setemail] = useState("");
+const [phoneNumber, setphoneNumber] = useState("");
+const [price, setprice] = useState("")
+
+
+///////////////
+    useEffect(async () => {
+        try {
+          console.log(token);
+          const findInvestors = await axios.get("http://localhost:5000/getInvestor", {
+              
+            headers: { authorization: "Bearer " + token },
+          });
+          setInvestors(findInvestors.data);
+          console.log(findInvestors.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }, []);
+
+      const addInvestors = async () => {
+        try {
+          const result1 = await axios.post("http://localhost:5000/addInvestors",
+            {
+                name: name,
+                email : email,
+                phoneNumber:phoneNumber,
+                price:price,
+             
+            },
+            {
+              headers: { authorization: "Bearer " + token },
+            }
+          );
+          const copiArray = [...Investors];
+          copiArray.push(result1.data);
+          setInvestors(copiArray);
+        } catch (error) {
+          console.log("errrorrr here");
+        }
+      };
+
+      /////////////////delete
+      const deleteInvestors = async (id, i) => {
+        try {
+          const severes = await axios.delete(`http://localhost:5000/deletInvestors/${id}`,
+            {
+              headers: { authorization: "Bearer " + token },
+            }
+          );
+          const copied = [...Investors];
+          copied.splice(i, 1);
+          setInvestors(copied);
+        } catch (error) {
+          console.log("error");
+        }
+      };
+
+
+      const FuncName1=(e)=>{
+        setname1(e.target.value); 
+      } ;
+      const FunceMail=(e)=>{
+        setemail(e.target.value); 
+      } ;
+      const FuncPhone=(e)=>{
+        setphoneNumber(e.target.value); 
+      } ;
+      const FuncPrice=(e)=>{
+        setprice(e.target.value); 
+      } ;
+
     return (
         <div>
-           <h2>hiy from Investors</h2> 
-        </div>
+        {Investors.map((elem, i) => {
+          return (
+            <div  key={elem._id}>
+              <p> {elem.name}</p>
+              <p> {elem.email}</p>
+              <p>{elem.phoneNumber}</p>
+              <p>{elem.price}</p>
+              <button onClick={() => { deleteInvestors(elem._id, i);}} >  delete{" "}</button> 
+             
+            </div>
+          );
+        })}
+        <input onChange={(e) => { FuncName1(e); }} placeholder="name"/>{" "}
+        <input onChange={(e) => { FunceMail(e); }} placeholder="email"/>{" "}
+        <input onChange={(e) => { FuncPhone(e); }} placeholder="phoneNumber"/>{" "}
+        <input onChange={(e) => { FuncPrice(e); }} placeholder="price"/>{" "}
+        <button onClick={() => {addInvestors();}}> add</button>{" "}
+      
+      </div>
     )
 }
--- src/components/Region.js
+++ src/components/Region.js
@@ -5,7 +5,12 @@ export default function Region({ token }) {
   const [name, setName] = useState("");
   const [description, setescription] = useState("");
   const [img, setImg] = useState("");
-
+  const [search, setsearch] = useState("");
+  const [updateN, setupdateN] = useState("");
+  const [updateD, setupdateD] = useState("");
+  const [updateimg, setupdateimg] = useState("");
+  const [toggle, setToggle] = useState(false);
+  //--------------------get Regin----------
   useEffect(async () => {
     try {
       console.log(token, "token here");
@@ -27,11 +32,10 @@ export default function Region({ token }) {
   const FuncImg = (e) => {
     setImg(e.target.value);
   };
-  //////////////////////////add////
+  //////////////////////////post////
   const addRegion = async () => {
     try {
-      const result = await axios.post(
-        "http://localhost:5000/addRegion",
+      const result = await axios.post("http://localhost:5000/addRegion",
         {
           name: name,
           description: description,
@@ -52,8 +56,7 @@ export default function Region({ token }) {
   /////////delete/////////
   const deleteRegions = async (id, i) => {
     try {
-      const severes = await axios.delete(
-        `http://localhost:5000/deletRegion/${id}`,
+      const severes = await axios.delete(`http://localhost:5000/deletRegion/${id}`,
         {
           headers: { authorization: "Bearer " + token },
         }
@@ -62,53 +65,108 @@ export default function Region({ token }) {
       copied.splice(i, 1);
       setRegions(copied);
     } catch (error) {
-      console.log("severes");
+      console.log("error");
     }
   };
+  ////////////update/////////
+  const UpDateRegion = async (id, i) => {
+    try {
+      const Modification = await axios.put(`http://localhost:5000/updateRegion/${id}`,
+        {
+          name: updateN,
+          description: updateD,
+          img: updateimg,
+        },
+        {
+          headers: { authorization: "Bearer " + token },
+        }
+      );
+      const ArrayCopied = [...Regions];
+      ArrayCopied[i]=Modification.data;
+      setRegions(ArrayCopied);
+      console.log(ArrayCopied);
+    
+    } catch (error) {
+      console.log("ereor her");
+    }
+  };
+  const UpDateName = (e) => {
+    setupdateN(e.target.value);
+  };
+  const UpDateDesc = (e) => {
+    setupdateD(e.target.value);
+  };
+  const UpDateImg = (e) => {
+    setupdateimg(e.target.value);
+  };
 
+  //------------------ssersh---------
+  const FiandRegin = (e) => {
+    setsearch(e.target.value);
+  };
+  const FuncSearsh = () => {
+    const FuncSearsh = Regions.filter((element) => {
+      if (element.name.toLowerCase().includes(search.toLocaleLowerCase())) {
+        return element;
+      }
+    });
+    setRegions(FuncSearsh);
+    return FuncSearsh;
+  };
   return (
     <>
+
+   
+    
+
+   
       <div>
+        <div>
+          <input  placeholder="search" onChange={(e) => { FiandRegin(e);}} />
+           <button onClick={() => { FuncSearsh(); }} > searsh </button>
+        </div>
+
+        <br />
+        <br />
         {Regions.map((element, i) => {
           return (
             <div key={element._id}>
-              {/* <h1>{element.user.name}</h1> */}
               <p>name:{element.name}</p>
-              <img src={element.img} alt="nooo img" />
+              <img src={element.img} alt="no img" />
               <p> description : {element.description}</p>
-              <button
-                onClick={() => {
-                  deleteRegions(element._id, i);
-                }}
-              >
-                delete
-              </button>
+              <button onClick={() => { deleteRegions(element._id, i);}} >  delete{" "}</button>
+              <button onClick={() => { UpDateRegion(element._id, i); }}> submet </button>
+            
+
+
+              <div>
+              <button onClick={() => { setToggle(!toggle); }}>update </button>
+              {toggle === true ? (
+                <form>
+                  <input onChange={(e) => { UpDateName(e); }}placeholder="updateName"/>{" "}
+                  <br />
+                  <input onChange={(e) => { UpDateDesc(e); }} placeholder="updateDescription" />{" "}
+                  <br />
+                  <input onChange={(e) => { UpDateImg(e);}}placeholder="updateImg" />{" "}
+                  <br />
+                </form>
+              ) : (
+                ""
+              )}
+            </div>
+            <br />
+
             </div>
           );
         })}
       </div>
-      <input
-        onChange={(e) => {
-          FuncName(e);
-        }}
-      />{" "}
-      <input
-        onChange={(e) => {
-          FuncDesc(e);
-        }}
-      />{" "}
-      <input
-        onChange={(e) => {
-          FuncImg(e);
-        }}
-      />{" "}
-      <button
-        onClick={() => {
-          addRegion();
-        }}
-      >
-        add{" "}
-      </button>
+      <input onChange={(e) => { FuncName(e); }} placeholder="name"/>{" "}
+      <input  onChange={(e) => { FuncDesc(e); }} placeholder="description"/>{" "}
+      <input onChange={(e) => { FuncImg(e); }}placeholder="img"/>{" "}
+      <button onClick={() => {addRegion();}}> add</button>{" "}
+
+
+
     </>
   );
 }
